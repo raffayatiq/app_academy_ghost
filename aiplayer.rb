f760a66ed314e5af0991edad7108357828e764c3
Raffay Atiq <58357644+raffayatiq@users.noreply.github.com>
2020-03-31 18:08:27 +0500

    Update aiplayer.rb

--- aiplayer.rb
+++ aiplayer.rb
@@ -31,6 +31,7 @@ class AiPlayer
 	end
 
 	def set_winning_moves(fragment, dictionary)
+		@winning_moves = []
 		alphabets = ("a".."z").to_a
 		alphabets.each do |alphabet|
 			possible_word = fragment + alphabet
@@ -48,6 +49,7 @@ class AiPlayer
 	end
 
 	def set_losing_moves(fragment, dictionary)
+		@losing_moves = []
 		alphabets = ("a".."z").to_a
 		alphabets.each do |alphabet|
 			possible_word = fragment + alphabet
